cb0d9350416993009e73f3e95df5f40350a041dc
Adrian Mustea <mustea.adrian95@gmail.com>
2018-04-24 00:50:56 +0300

    init commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,16 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!
 
   def home
   end
+
+  def about
+  end
+
+  def news
+  end
+
+  def contact
+  end
+
 end
